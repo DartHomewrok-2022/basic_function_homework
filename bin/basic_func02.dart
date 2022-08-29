0fd172f33d250f78155dba1e8be927a2b898b1e1
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-29 09:59:51 +0500

    Update basic_func02.dart

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,4 @@
-//  Create a function called func.
-
-//  Return the value "Hello World"
+// Create a function called divide. 
+//Create function arguments a and b of type int. 
+//Returns the floor division of a and b.
 void main() {}
