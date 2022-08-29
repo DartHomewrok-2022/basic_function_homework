b1a9aaa88d1df1777f664a6ab4b391b786bc297b
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-29 10:33:36 +0500

    Update basic_func05.dart

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -1,4 +1,4 @@
 // Create a function called func.
-
-// Return the value double type.
+// Create a fucntion argument ‘number’ two digits numbers will be given.
+// return the sum of the number’s digits.
 void main() {}
