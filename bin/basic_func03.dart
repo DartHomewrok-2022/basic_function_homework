a302af20a5e9c0f919d66f2d1eb62a978c625d03
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-29 10:29:21 +0500

    Update basic_func03.dart

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -1,4 +1,4 @@
-// Create a function called func.
-
-// Return the value "codeschooluz"
+//Create a function called func. 
+//Create function arguments a and b of type int. 
+//Returns the remainder of a and b.
 void main() {}
