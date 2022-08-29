208ff2e7e0c6333ee4974a5cd1641d919d2753e1
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-29 10:35:06 +0500

    Update basic_func06.dart

--- bin/basic_func06.dart
+++ bin/basic_func06.dart
@@ -1,4 +1,4 @@
-// Create a function called func.
-
-// Return the value String type.
+// Create a function called func. 
+// Create a function argument called ‘number’ of type int the two-digit numbers will be given. 
+// Find the reverse of the number and return to a variable called ‘answer’ return answer
 void main() {}
