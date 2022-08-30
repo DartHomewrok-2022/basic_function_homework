20c4ba8c0c1ed6ffcd63d0bedc57538f5107c5bf
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-30 08:49:49 +0500

    Update basic_func07.dart

--- bin/basic_func07.dart
+++ bin/basic_func07.dart
@@ -1,6 +1,8 @@
 //  Create a function called func.
-
-//  Create function arguments a.
-
-//  Return the value a.
+// Create a function argument called ‘number’ of type int the three-digit number will be given.
+// Find the ‘number’ first digit and assign to x1.
+// Find the ‘number’ second digit and assign to x2. 
+//  Find the ‘number’ third digit and assign to x3.
+// Create a variable called ‘answer’ and assign it the sum of the three digits. 
+// return the sum of the digits.
 void main() {}
