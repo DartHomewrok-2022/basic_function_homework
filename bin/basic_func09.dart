626b46b4fb2747fb8baa10cc97e7b4f4ffe9920d
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-08-30 08:55:25 +0500

    Update basic_func09.dart

--- bin/basic_func09.dart
+++ bin/basic_func09.dart
@@ -1,6 +1,5 @@
-//  Create a function called func.
-
-//  Create function arguments a.
-
-//  decrease the value of a to one and return
+// Create a function called func.
+// Create a function argument called ‘number’ of type int.
+// Find the remainder of the division of number by 2 and assign it to ‘answer’.
+// return the answer.
 void main() {}
